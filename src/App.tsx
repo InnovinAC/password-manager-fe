39132f5fd44e90d429338f0e36296a91dc0e4247
innovin <innovinanuonye@gmail.com>
2023-06-17 02:57:48 +0100

    base setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './index.css'
+import Navbar from "./components/navbar";
+
+function App() {
+
+
+    return (
+        <>
+            <Navbar/>
+            <div className="flex items-center flex-col justify-center h-screen">
+                <h1 className="text-4xl font-bold text-center">Welcome to password manager By Innovin. Sit back and enjoy what's cooking!&#x1F9D1;&#x200D;&#x1F373;&#x1F525;</h1>
+                <p className="text-lg font-bold text-center">Trust me, you're gonna love it!</p>
+            </div>
+
+        </>
+    )
+}
+export default App;
