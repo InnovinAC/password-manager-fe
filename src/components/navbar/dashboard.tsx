12f8c656d80bd117072052805d4adbb92443dcc4
innovin <innovinanuonye@gmail.com>
2023-06-20 21:37:30 +0100

    update

--- src/components/navbar/dashboard.tsx
+++ src/components/navbar/dashboard.tsx
@@ -9,9 +9,9 @@ const DashboardNavbar = () => {
                 <div className={'flex'}>
                     <img className={'w-12 h-12 rounded-full mr-3 overflow-hidden'}
                          src={Img}/>
-                    <div className={"flex space-y-0 flex-col"}>
+                    <div className={"flex flex-col"}>
                         <span>Welcome Back</span>
-                        <span className={"font-bold"}>Innovin</span>
+                        <span className={"font-bold text-lg"}>Innovin</span>
                     </div>
                 </div>
                 <div className={'flex my-auto space-x-3 flex-row'}>
